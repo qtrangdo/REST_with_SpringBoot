041f2616926f484dcedff6314273e9a2213444ce
Trang Do <trang.do@slalom.com>
2019-06-07 10:01:29 -0400

    Flesh out GET requests

--- hello-world/src/main/java/com/example/helloworld/controllers/UserController.java
+++ hello-world/src/main/java/com/example/helloworld/controllers/UserController.java
@@ -2,18 +2,26 @@ package com.example.helloworld.controllers;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users") //http://localhost:3000/users
 public class UserController {
 
-  @GetMapping
-  public String getUser() {
-    return "getUser was called";
+  @GetMapping()
+  public String getUsers(@RequestParam(value="page", defaultValue = "1") int page, @RequestParam(value="limit", defaultValue = "30") int limit) {
+    return "getUsers was called with params: page - " + page + " limit - " + limit;
+  }
+  //RequestParam with no defaultValue will be required info.
+
+  @GetMapping(path="/{userId}")
+  public String getUser(@PathVariable String userId) {
+    return "getUser was called with Id " + userId;
   }
 
   @PostMapping
