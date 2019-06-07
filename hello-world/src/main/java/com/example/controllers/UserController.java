d4375b0fb516ca67c7809875d12c7dee707da7ba
Trang Do <trang.do@slalom.com>
2019-06-07 09:21:56 -0400

    Template for POST and PUT

--- hello-world/src/main/java/com/example/controllers/UserController.java
+++ hello-world/src/main/java/com/example/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,15 @@ public class UserController {
   @GetMapping
   public String GetUser() {
     return "GetUser was called";
+  }
+
+  @PostMapping
+  public String CreateUser() {
+    return "CreateUser was called";
+  }
+
+  @PutMapping
+  public String UpdateUser() {
+    return "UpdateUser was called";
   }
 }
