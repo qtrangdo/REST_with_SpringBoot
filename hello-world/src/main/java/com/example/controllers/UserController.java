4d190c7cc1ba4acee97f6d500a06580b76beabf5
Trang Do <trang.do@slalom.com>
2019-06-07 09:17:07 -0400

    Create controller folder

---
+++ hello-world/src/main/java/com/example/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.example.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") //http://localhost:3000/users
+public class UserController {
+  
+}
