b786c50c2f1961d983fb8c2a85749ce3fb942e42
Trang Do <trang.do@slalom.com>
2019-06-07 09:19:17 -0400

    Template for get request

--- hello-world/src/main/java/com/example/controllers/UserController.java
+++ hello-world/src/main/java/com/example/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.example.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users") //http://localhost:3000/users
 public class UserController {
-  
+
+  @GetMapping
+  public String GetUser() {
+    return "GetUser was called";
+  }
 }
